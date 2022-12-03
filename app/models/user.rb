17d486b3c12f965b0c2226a28451b90f7f33c031
EC2 Default User <ec2-user@ip-172-31-94-93.ec2.internal>
2022-12-03 04:16:03 +0000

    [Finish]応用課題４

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
+  # ↑は、has_many :books, class_name: 'Book', foreign_key: 'book_id', dependent: :destroy と同じ
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
+
+  has_many :relationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
+  has_many :reverse__of_relationships, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
+
+
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse__of_relationships, source: :follower
+
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
@@ -15,6 +23,19 @@ class User < ApplicationRecord
 
 
   def get_profile_image
-    (profile_image.attached?) ? profile_image : 'no_image.jpg'
+    (profile_image.attached?)? profile_image : 'no_image.jpg'
   end
+
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
+
 end
